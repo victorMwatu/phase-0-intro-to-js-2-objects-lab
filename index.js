0be1452747d97c70efd2b795c72a35ab888b560a
victorMwatu <victormwatu@gmail.com>
2025-05-28 21:24:40 +0300

    complete intro-to-js-2-objects-lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+//create employee obj
+const employee = {name: "Jonte", streetAddress: "Eastest of lands"};
+//update employee non destructive
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]: value};
+}
+//update employee destructively
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+//delete from employees
+function deleteFromEmployeeByKey(employee, key) {
+    const {[key] : _, ...rest } = employee;
+    return rest;
+
+}
+//delete from employees destructively
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
