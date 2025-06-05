5d0294a17c7e5c3cddbf22e6108dab34b5fc4c7c
victorMwatu <60965557+victorMwatu@users.noreply.github.com>
2025-06-05 11:56:01 +0300

    again!!!

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-//create employee obj
-const employee = {name: "Jonte", streetAddress: "Eastest of lands"};
-//update employee non destructive
+
+const employee = {name: "Henry", streetAddress: "ParkLane"};
+
 function updateEmployeeWithKeyAndValue(employee, key, value) {
     return {...employee, [key]: value};
 }
-//update employee destructively
+
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value;
     return employee;
 }
-//delete from employees
+
 function deleteFromEmployeeByKey(employee, key) {
     const newobj = {...employee};
     delete newobj[key];
     return newobj;
 
 }
-//delete from employees destructively
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
     return employee;
